7ab7bf2fbc6eae29284817ca7ede0c522ce22bcc
igsprase0574 <prasetyo.putranto@ai.astra.co.id>
2025-11-23 16:09:27 +0700

    add data layer

---
+++ tests/data/login.data.js
@@ -0,0 +1,14 @@
+export const loginDataSet = {
+  validUser: {
+    firstName: "admin",
+    lastName: "admin",
+    username: "adminqa4",
+    password: "Camelcase01!",
+  },
+  invalidUser: {
+    firstName: "",
+    lastName: "",
+    username: "nope",
+    password: "123",
+  },
+};
---
+++ tests/data/register.data.js
@@ -0,0 +1,14 @@
+export const registerDataSet = {
+  validUser: {
+    firstName: "admin",
+    lastName: "admin",
+    username: "adminqa4",
+    password: "Camelcase01!",
+  },
+  invalidUser: {
+    firstName: "",
+    lastName: "",
+    username: "nope",
+    password: "123",
+  },
+};
